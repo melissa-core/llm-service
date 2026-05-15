d99b55d248ea15aad09152562f55dd92217720bc
Jahongir Zokirov <zokirov123404321@gmail.com>
2026-05-15 16:14:14 +0500

    Fixed swagger config

--- src/main/java/uz/melisa/config/SwaggerConfig.java
+++ src/main/java/uz/melisa/config/SwaggerConfig.java
@@ -17,9 +17,9 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Melisa")
+                        .title("Melissa")
                         .version("1.0")
-                        .description("Melisa API"))
+                        .description("Melissa API"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEME_NAME,
